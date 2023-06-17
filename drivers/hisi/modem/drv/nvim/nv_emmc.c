4b6ed76af906d2cae00be3f5793ba601eb68cafb
R0rt1z2 <me@r0rt1z2.com>
2023-06-17 22:03:12 +0000

    drivers: hisi: mode: Remove invalid self assignment(s)

--- drivers/hisi/modem/drv/nvim/nv_emmc.c
+++ drivers/hisi/modem/drv/nvim/nv_emmc.c
@@ -885,8 +885,7 @@ u32 nv_emmc_access(const s8* path,s32 mode)
     {
         return NV_ERROR;
     }
-	/* coverity[self_assign] */
-    mode = mode;
+
     switch(fd->emmc_type)
     {
         case NV_FILE_DLOAD:
