027a0873f737cdfd02639cd8a631cbe2dacbedc0
Huawei-Dev <surdu_petru@yahoo.com>
2023-11-11 15:58:39 +0100

    remove illegal characters from nv_base.c

--- drivers/hisi/modem/include/nv/product/product_nv_def.h
+++ drivers/hisi/modem/include/nv/product/product_nv_def.h
@@ -24,14 +24,14 @@ extern "C" {
 
 #ifndef __u8_defined
 #define __u8_defined
-typedef signed char          s8;
+/*typedef signed char          s8;
 typedef unsigned char        u8;
 typedef signed short         s16;
 typedef unsigned short       u16;
 typedef signed int           s32;
 typedef unsigned int         u32;
 typedef signed long long     s64;
-typedef unsigned long long   u64;
+typedef unsigned long long   u64;*/
 #endif
 
 /*请参考结构体样例*/
