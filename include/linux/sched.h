e60b71af756e9b5901e031c93feca04bbd4faafe
Huawei-Dev <surdu_petru@yahoo.com>
2023-12-16 11:52:13 +0100

    revert commit: softirq: defer softirq processing to ksoftirqd if CPU is busy with RT : 45afcf5

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2575,7 +2575,6 @@ extern void do_set_cpus_allowed(struct task_struct *p,
 
 extern int set_cpus_allowed_ptr(struct task_struct *p,
 				const struct cpumask *new_mask);
-extern bool cpupri_check_rt(void);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p,
 				      const struct cpumask *new_mask)
@@ -2587,10 +2586,6 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p,
 	if (!cpumask_test_cpu(0, new_mask))
 		return -EINVAL;
 	return 0;
-}
-static inline bool cpupri_check_rt(void)
-{
-	return false;
 }
 #endif
 
